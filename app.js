791e4a04a7cb0b455aee03808653ab8c5434aef9
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2018-03-13 01:22:39 -0500

    pasando hacia el repositorio

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
+var index = require('./routes/index');
+var users = require('./routes/users');
+//view engine
+var hbs = require('express-handlebars');
+//session user
+var session = require('express-session');
+//passport
+var passport = require('passport');
+var flash = require('connect-flash');
+//db
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/test', function(err){
+  if(err) return console.log(err);
+});
+require('./config/passport');
+
+// view engine setup
+app.engine('hbs', hbs(
+  {extname: 'hbs', defaultLayout: 'main',
+   partialsDir:__dirname + '/views/partials',
+   layoutsDir: __dirname + '/views/layouts'})
+  );
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//session init
+app.use(session({
+  secret: 'mysupersecret',
+  resave: false,
+  saveUninitialized: false
+}));
+//flash init
+app.use(flash());
+//passport init
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ seed/product-seeder.js
@@ -0,0 +1,43 @@
+var Product = require('../models/product');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', function (err) {
+    if (err) return console.log(err);
+});
+
+var products =[
+    new Product({
+        imagePath: 'https://cdn.shopify.com/s/files/1/2313/5135/products/Extra_Thicc_ALT_2000x.png?v=1511317503',
+        title: 'Thicc Body',
+        description: 'Just for thicc people',
+        price: 10
+    }),
+    new Product({
+        imagePath: 'https://cdn.shopify.com/s/files/1/1310/3983/products/BAD-SEED---T-SHIRT_2048x2048.png?v=1480347032',
+        title: 'EXTRA TICC',
+        description: 'Just for thicc people',
+        price: 25
+    }),
+    new Product({
+        imagePath: 'https://mms-cloudfront.customink.com/mms/images/catalog/99af4637877c2698d1b5f8faefa4751a/colors/169900/views/alt/front_large_extended.png?ixlib=rails-2.1.4&w=412&h=470&fit=crop&dpr=1&bg=ffffff&fm=pjpg&q=39&auto=compress',
+        title: 'Simple like this',
+        description: 'Just for thicc people',
+        price: 19
+    })
+];
+
+var done = 0;
+for (var i = 0; i < products.length; i++) {
+    products[i].save(function(err, result){
+        done++;
+        if( done === products.length){
+            exit();
+        }
+    });
+    
+}
+function exit(){
+    mongoose.disconnect(function (err) {
+        if (err) return console.log(err);
+    })
+}
